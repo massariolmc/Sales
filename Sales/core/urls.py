16164fb8ebab3c920ba1262f82e79164e1b3eea3
Maximiano Massariol Cardoso <massariolmc@gmail.com>
2020-07-01 21:43:17 -0400

    Still fixing

--- Sales/core/urls.py
+++ Sales/core/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'signup'
+
 urlpatterns = [
     path('home', views.home, name='home'),
-    path('signup/', views.signup, name = 'signup'),
+    path('signup-create/', views.signup, name = 'url_signup-create'),
+    path('signup/', views.signup_list, name='url_signup_list'),
+    path('<int:pk>/delete', views.signup_delete, name='url_signup_delete'),
+    path('<int:pk>/detail', views.signup_detail, name='url_signup_detail'),
+    path('<int:pk>/edit', views.signup_edit, name='url_signup_edit'),
 ]
--- Sales/core/views.py
+++ Sales/core/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 # USER
 from Sales.account.forms import UserCustomCreateForm
 from Sales.account.models import User
@@ -14,8 +15,43 @@ def signup(request):
         form = UserCustomCreateForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect('signup:url_signup_list')
     else:
         form = UserCustomCreateForm()
        
-    return render(request, 'core/signup.html',{'form': form})
+    return render(request, 'core/signup_form.html',{'form': form})
+
+def signup_list(request):
+    template_name = 'core/signup_list.html'
+    obj = User.objects.all()
+    data = {
+        'objects': obj
+    }
+    return render(request,template_name,data)
+
+def signup_edit(request,pk):
+    template_name = 'core/signup_form.html'    
+    obj = get_object_or_404(User, pk=pk)
+    form = UserCustomCreateForm(request.POST or None, instance=obj)
+    if request.method == 'POST':        
+        if form.is_valid():
+            form.save()
+            return redirect('signup:url_signup_list')       
+    data = { 'form': form}
+    return render(request, template_name,data)
+
+def signup_detail(request,pk):
+    template_name = 'core/signup_detail.html'
+    obj = User.objects.get(pk=pk)
+    data = {
+        'object': obj
+    }
+    return render(request, template_name,data)
+
+def signup_delete(request,pk):
+    #obj = get_object_or_404(User, pk=pk)
+    #obj.delete()
+    messages.success(request, 'Ação concluída com sucesso.')    
+    return redirect('signup:url_signup_list')
+
+
