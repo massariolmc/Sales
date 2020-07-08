59da6c5916c92f15031b6962052f63a0a9010064
Maximiano Massariol Cardoso <massariolmc@gmail.com>
2020-07-08 19:01:05 -0400

    Fix Javascript search and show

--- Sales/core/urls.py
+++ Sales/core/urls.py
@@ -5,7 +5,7 @@ app_name = 'signup'
 
 urlpatterns = [
     path('home', views.home, name='home'),
-    path('signup-create/', views.signup, name = 'url_signup-create'),
+    path('signup-create/', views.signup, name = 'url_signup_create'),
     path('signup/', views.signup_list, name='url_signup_list'),
     path('<int:pk>/delete', views.signup_delete, name='url_signup_delete'),
     path('<int:pk>/detail', views.signup_detail, name='url_signup_detail'),
--- Sales/core/views.py
+++ Sales/core/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+
 # USER
 from Sales.account.forms import UserCustomCreateForm
 from Sales.account.models import User
+from django.db.models import Q
 
 # Create your views here.
 def home(request):
@@ -24,9 +29,42 @@ def signup(request):
 def signup_list(request):
     template_name = 'core/signup_list.html'
     obj = User.objects.all()
+    data = dict()    
+
+    def pagination(request,obj,lines=10):
+        page = request.GET.get('page', 1)
+        paginator = Paginator(obj, int(lines))        
+        try:
+            users = paginator.page(page)
+        except PageNotAnInteger:
+            users = paginator.page(1)
+        except EmptyPage:
+            users = paginator.page(paginator.num_pages)
+        
+        return users
+
+    if request.is_ajax():
+        query = request.GET.get('q')
+        lines = request.GET.get('l')        
+        if query:
+            obj = User.objects.filter(
+                Q(cpf__icontains=query) | Q(first_name__icontains=query) |
+                Q(last_name__icontains=query) | Q(email__icontains=query) | Q(created_at__icontains=query)
+            ).distinct() 
+        else:
+            print("Não existe") 
+
+        users = pagination(request,obj,int(lines))                           
+    
+        data['html_signup_list'] = render_to_string('core/_signup_table_list.html', {'objects': users})       
+        return JsonResponse(data)
+            
+                 
+    lines = 2
+    users = pagination(request,obj,int(lines))   
     data = {
-        'objects': obj
-    }
+        'objects': users
+    }    
     return render(request,template_name,data)
 
 def signup_edit(request,pk):
@@ -51,7 +89,8 @@ def signup_detail(request,pk):
 def signup_delete(request,pk):
     #obj = get_object_or_404(User, pk=pk)
     #obj.delete()
-    messages.success(request, 'Ação concluída com sucesso.')    
+    messages.success(request, 'Ação concluída com sucesso.')
+    #messages.error(request, 'Ação concluída com sucesso.')    
     return redirect('signup:url_signup_list')
 
 
