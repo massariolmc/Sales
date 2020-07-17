b9b85cb50b99604ab036dcee90d40a5673f1548b
Maximiano Massariol Cardoso <massariolmc@gmail.com>
2020-07-17 18:07:26 -0400

    Finish all Templates User

--- Sales/account/forms.py
+++ Sales/account/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.forms import ModelForm, TextInput, DateInput, Select, RadioSelect, SelectDateWidget, HiddenInput, PasswordInput, EmailInput
 from django.utils.translation import ugettext, ugettext_lazy as _
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
-
+from django.core.exceptions import ValidationError
 from .models import User
 
 
@@ -11,6 +11,12 @@ class UserCustomCreateForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("cpf", "first_name", "last_name", "username", "email", "password1", "password2")
+    
+    def clean_cpf(self): # No caso aqui clean_nome_do_campo        
+        cpf = self.cleaned_data['cpf']
+        if not cpf.isdigit():                              
+            raise ValidationError("Aceita apenas números.")
+        return name# sempre retornar um dado, de preferencia o valor que estava no campo.
 
 class UserChangeForm(UserChangeForm):
     class Meta:
